5b2c17fb4f36385b273dff885094ef4f6dce5022
Santos Vilanculos <95357132+SantosVilanculos@users.noreply.github.com>
2025-04-01 23:28:39 +0200

    chore: update install.sh

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ if [ -f ./composer.lock ]; then
     php artisan vendor:publish --tag=log-viewer-assets
 
     touch ./database/database.sqlite
-    php artisan migrate:fresh --seed || php artisan migrate --seed --graceful
+    php artisan migrate --seed
 fi
 
 pnpm install
