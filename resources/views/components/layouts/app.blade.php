5600c9956d5876a6b8e089459ea952ed736c0ca5
Santos Vilanculos <95357132+SantosVilanculos@users.noreply.github.com>
2025-04-05 15:03:45 +0200

    chore: add open graph tags

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -10,8 +10,20 @@
         <link rel="icon" href="{{ asset('favicon.ico') }}" sizes="48x48 32x32 16x16" type="image/x-icon" />
         <link rel="icon" href="{{ asset('favicon.svg') }}" sizes="any" type="image/svg+xml" />
         <title>{{ config('app.name') }}</title>
+        <meta name="description" content="" />
 
+        <meta name="og:title" content="{{ config('app.name') }}" />
+        <meta name="og:description" content="" />
+        <meta name="og:type" content="website" />
+        <meta name="og:url" content="{{ config('app.url') }}" />
+        <meta name="og:image" content="{{ url('og.png') }}" />
+        <meta name="og:image:type" content="image/png" />
+        <meta name="og:image:width" content="1200" />
+        <meta name="og:image:height" content="630" />
 
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="" />
+        <meta name="twitter:creator" content="" />
         <link rel="stylesheet" href="{{ asset('vendor/inter/4.1/inter.min.css') }}" />
 
         @livewireStyles
