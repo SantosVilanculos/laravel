edb1190e59f319c172275eb905516168a92fd109
Santos Vilanculos <95357132+SantosVilanculos@users.noreply.github.com>
2025-04-25 19:25:42 +0200

    test(arch): don't describe

--- tests/Unit/ArchTest.php
+++ tests/Unit/ArchTest.php
@@ -2,18 +2,16 @@
 
 declare(strict_types=1);
 
-describe('arch', function (): void {
-    arch()->preset()->php();
-    arch()->preset()->security();
-    arch()->preset()->laravel();
+arch()->preset()->php();
+arch()->preset()->security();
+arch()->preset()->laravel();
 
-    arch()->expect('App')->toUseStrictTypes();
-    arch()->expect('Database\Factories')->toUseStrictTypes();
-    arch()->expect('Database\Seeders')->toUseStrictTypes();
+arch()->expect('App')->toUseStrictTypes();
+arch()->expect('Database\Factories')->toUseStrictTypes();
+arch()->expect('Database\Seeders')->toUseStrictTypes();
 
-    arch()->expect('App')->classes()->not->toBeFinal();
-    arch()->expect('Database\Factories')->classes()->not->toBeFinal();
-    arch()->expect('Database\Seeders')->classes()->not->toBeFinal();
+arch()->expect('App')->classes()->not->toBeFinal();
+arch()->expect('Database\Factories')->classes()->not->toBeFinal();
+arch()->expect('Database\Seeders')->classes()->not->toBeFinal();
 
-    arch()->expect('App\Http\Controllers')->toExtendNothing();
-});
+arch()->expect('App\Http\Controllers')->toExtendNothing();
