59ce82956bc5c886b84c9d616671f660619a1605
Santos Vilanculos <95357132+SantosVilanculos@users.noreply.github.com>
2025-04-28 18:57:05 +0200

    feat: add User model columns property types

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,6 +9,16 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+/**
+ * @property-read int $id
+ * @property-read string $name
+ * @property-read string $email
+ * @property-read \Carbon\CarbonImmutable|null $email_verified_at
+ * @property-read string $password
+ * @property-read string|null $remember_token
+ * @property-read \Carbon\CarbonImmutable|null $created_at
+ * @property-read \Carbon\CarbonImmutable|null $updated_at
+ */
 class User extends Authenticatable
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
