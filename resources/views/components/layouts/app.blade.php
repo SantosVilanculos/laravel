d6bf890b33ad37f45983a0d3e0d576df74c403f5
Santos Vilanculos <santosvilanculos@yahoo.com>
2025-07-02 03:23:15 +0200

    chore: remove inter font local link

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -26,8 +26,6 @@
         <meta name="twitter:site" content="" />
         <meta name="twitter:creator" content="" />
 
-        <link rel="stylesheet" href="{{ asset('vendor/inter/4.1/inter.min.css') }}" />
-
         @livewireStyles
 
         @if (file_exists(public_path('build/manifest.json')) || file_exists(public_path('hot')))
@@ -146,7 +144,7 @@
 
                                     {{--
                                         Dropdown menu, show/hide based on menu state.
-                                        
+
                                         Entering: "transition ease-out duration-100"
                                         From: "transform opacity-0 scale-95"
                                         To: "transform opacity-100 scale-100"
